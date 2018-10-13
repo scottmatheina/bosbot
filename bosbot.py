6f43c219d737d5d73c0dd7ed0c099931b6424a15
Scott Matheina <scott@matheina.com>
2018-10-13 13:36:27 -0500

    update

--- bosbot.py
+++ bosbot.py
@@ -1,27 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater
-updater = Updater(token='TOKEN')
+updater = Updater(token='672249649:AAFVgl1vI5cZ83qp_Zm1gkFmCYAMiKZw7mQ')
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
                      
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")     
-
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-
-def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
 updater.start_polling()
-              
