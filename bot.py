a0f6ca9aafb5d53bfb440cb7e96b4fe32f07781b
Scott Matheina <scott@matheina.com>
2018-10-16 00:10:33 -0500

    testing

--- bot.py
+++ bot.py
@@ -4,13 +4,15 @@
 import os
 token = os.environ.get("TOKEN")
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
 from telegram.ext import Updater, CommandHandler
-
 
 def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
-
 
 updater = Updater(token)
 
