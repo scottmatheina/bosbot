25f296d661d3556bba9d6cc708e2e341b0c42f25
Scott Matheina <scott@matheina.com>
2018-10-21 11:13:32 -0500

    added delete all commands

--- bot.py
+++ bot.py
@@ -11,8 +11,13 @@ logging.basicConfig(level=logging.DEBUG,
 		    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 updater = Updater(token)
+def delete_command(bot, update):
+	update.message.delete()
 
-def delete(bot, update):
+delete_command_handler = MessageHandler([Filters.command], delete_command)
+updater.dispatcher.add_handler(delete_command_handler)
+
+def delete_ban(bot, update):
 	message = update.message.text.lower()
 	ban = ['retard',
 		'retarded',
@@ -31,8 +36,8 @@ def delete(bot, update):
 		if i in message:
 			update.message.delete()
 
-delete_handler = MessageHandler(Filters.text, delete)
-updater.dispatcher.add_handler(delete_handler)
+delete_ban_handler = MessageHandler(Filters.text, delete_ban)
+updater.dispatcher.add_handler(delete_ban_handler)
 
 updater.start_polling()
 updater.idle()
