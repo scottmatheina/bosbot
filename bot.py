6ae0b440db56418bc655eff026c8ae83494bfdde
Scott Matheina <scott@matheina.com>
2018-10-23 06:48:50 -0500

    comment out welcome message

--- bot.py
+++ bot.py
@@ -12,21 +12,21 @@ logging.basicConfig(level=logging.DEBUG,
 
 updater = Updater(token)
 
-def welcome(bot, update):
-	for new_user_obj in update.message.new_chat_members:
-		chat_id = update.message.chat.id
-		new_user = ""
-
-		try:
-			new_user = "@" + new_user_obj['username']
-
-		except Exception as e:
-			new_user = new_user_obj['first_name']
-
-		bot.send_message(chat_id=chat_id, text=" %s Welcome!" % new_user)
-
-welcome_handler = MessageHandler(Filters.status_update.new_chat_members, welcome)
-updater.dispatcher.add_handler(welcome_handler)
+#def welcome(bot, update):
+#	for new_user_obj in update.message.new_chat_members:
+#		chat_id = update.message.chat.id
+#		new_user = ""
+#
+#		try:
+#			new_user = "@" + new_user_obj['username']
+#
+#		except Exception as e:
+#			new_user = new_user_obj['first_name']
+#
+#		bot.send_message(chat_id=chat_id, text=" %s Welcome!" % new_user)
+#
+#welcome_handler = MessageHandler(Filters.status_update.new_chat_members, welcome)
+#updater.dispatcher.add_handler(welcome_handler)
 
 def delete_command(bot, update):
 	update.message.delete()
