f746f5505fc587a8516a3d759c7dcde1758ac1cb
Scott Matheina <scott@matheina.com>
2018-10-28 07:29:09 -0500

    welcome message, program files

--- bot.py
+++ bot.py
@@ -2,31 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import os
-
+import random
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
+from msg_list import message_list
 token = os.environ.get("TOKEN")
 logging.basicConfig(level=logging.DEBUG,
 		    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 updater = Updater(token)
 
-#def welcome(bot, update):
-#	for new_user_obj in update.message.new_chat_members:
-#		chat_id = update.message.chat.id
-#		new_user = ""
+#def announcments(bot, job):
+#	chat_id = update.message.chat.id
+#	message_list = random.choice([
+#			'text1',
+#			'text2',
+#			'text3'
+#			])
 #
-#		try:
-#			new_user = "@" + new_user_obj['username']
-#
-#		except Exception as e:
-#			new_user = new_user_obj['first_name']
-#
-#		bot.send_message(chat_id=chat_id, text=" %s Welcome!" % new_user)
-#
-#welcome_handler = MessageHandler(Filters.status_update.new_chat_members, welcome)
-#updater.dispatcher.add_handler(welcome_handler)
+#	bot.send_message(chat_id=chat_id, text='textkksks')
+
+#updater.job_queue.run_repeating(announcments, interval=5, first=0)
 
 def delete_command(bot, update):
 	update.message.delete()
@@ -55,6 +51,25 @@ def delete_ban(bot, update):
 
 delete_ban_handler = MessageHandler(Filters.text, delete_ban)
 updater.dispatcher.add_handler(delete_ban_handler)
+
+def welcome(bot, update):
+
+	for new_user_obj in update.message.new_chat_members:
+		chat_id = update.message.chat.id
+
+		new_user = ""
+
+		try:
+			new_user = "@" + new_user_obj['username']
+
+		except Exception as e:
+			new_user = new_user_obj['first_name']
+
+		message = random.choice(message_list)
+		bot.send_message(chat_id=chat_id, text="{0} Welcome! {1}".format(new_user, message))
+
+welcome_handler = MessageHandler(Filters.status_update.new_chat_members, welcome)
+updater.dispatcher.add_handler(welcome_handler)
 
 updater.start_polling()
 updater.idle()
---
+++ msg_list.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+message_list = [
+'The updated Roadmap is published on the forum at the below\
+ link. https://forum.boscoin.io/t/boscoin-roadmap/2195',
+##
+'Article, 10 May 2018, Overview of White Paper 2.0 Part 1\
+\nhttps://boscoin.io/article/overview-of-white-paper-2-part1\
+\nIf you have not read this, it is a good overview of the white\
+ paper 2.0 part 1 in compliance review.',
+##
+'If you are not already subscribed to the Official BOScoin\
+ Announcements Telegram now is a great time. Receive all the\
+ important announcements in an uncluttered channel.\
+\nhttps://t.me/OfficialBOScoinAnnouncements',
+##
+'We have published a one page brochure covering basic platform information.\
+\nhttps://boscoin.io/data/brochure.html',
+##
+'You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
+ the community. The exact procedure, and more information will be released as we\
+ get closer to MainNet Launch near the end of Q4, 2018.\
+\nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe',
+##
+'Follow the development on github by “watching” the repository\
+\nhttps://github.com/bosnet/sebak\
+\nselecting email, and you’ll receive all the development updates.',
+##
+'Article and Application, "Reverse ICO Partner Program"\
+\nhttps://boscoin.io/pf/\
+\n\
+What is RIPP (Reverse ICO Partner Program)?\
+\n- RIPP is financed by BOSNet, the mainnet for public financing based on blockchain.\
+\n- RIPP is targeting reverse-ICO firms that are already operational with their own business\
+ models, and looking for additional financing through blockchain based ICOs or Public Financing,\
+ not the traditional financial system.\
+\n- Our partners can conduct business quickly, without the need for extensive technological\
+ investments into the blockchain. Furthermore, raising post-ICO capital on BOSNet through\
+ Public Financing is drastically cheaper than using the traditional financial system with\
+ its high fees.',
+##
+'We have published a one page brochure covering basic platform information.\
+\nhttps://boscoin.io/data/brochure.html\
+\n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
+\n- BOScoin offers three alternatives - credit creation through participation, democratic impact\
+ investments and communization of certain assets to form a new "Project Financing (PF)" concept to\
+ power better capitalism, solving the high interest rate problem in conventional PF.\
+\n- BOScoin plans to release "BOSnet", a blockchain based mainnet optimized for PF by Q4 of 2018.\
+\n- BOScoin is running a "Reverse ICO Partner Program" to discover and foster companies in need of\
+ PF through reverse ICOs.',
+##
+'Exchanges\
+\n- Hitbtc - Do not buy from this exchange, they are trading an "IOU" withdrawals and deposits\
+ are not allowed.\
+\n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.co.kr are the only exchanges\
+ trading BOScoin at this time.',
+##
+'Monthly Ask Me Anything with CEO and CTO\
+\nSave your questions and submit them when the question period opens on the 1st.\
+\n- Submit questions 1st -10th\
+\n- Team compiles answers 11th - 20th\
+\n- Publishes answers on boscoin.io 21st - 31st\
+\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform',
+##
+'Article, 10 May 2018, "Overview of White Paper 2.0"\
+\nhttps://boscoin.io/article/overview-of-white-paper-2-part1/\
+\nIf you have not read this, it is a good overview of the white paper 2.0 in compliance review.',
+##
+'The updated Roadmap is published on the forum at the below link.\
+\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195'
+##
+'We have several official channels for up to date information on our project. We recommend you use\
+ these to stay informed regarding current events in the BOScoin eco-system.\
+\n\
+\n- Twitter www.twitter.com/boscoinio\
+\n- Community Forum - https://forum.boscoin.io/\
+\n- Telegram announcements - https://t.me/OfficialBOScoinAnnouncements\
+\n- Medium Blog - https://medium.com/@boscoin\
+\n- Github repositories - https://github.com/bosnet\
+\n- Official Youtube channel - https://www.youtube.com/channel/UC8jcLGbwx_itHzgFPcTLiBg',
+##
+'We have released the TokenNet Android Mobile Wallet on the Google Play store.\
+\nhttps://play.google.com/store/apps/details?id=io.boscoin.toknenet.wallet\
+\n\
+\nThe iOS version is scheduled for Q1, 2019.'
+]
