68b9b9c7c7859f33ec3dc8c4591be3801446bf0d
Scott Matheina <scott@matheina.com>
2018-10-28 10:32:16 -0500

    updated message list

--- msg_list.py
+++ msg_list.py
@@ -3,32 +3,32 @@
 
 message_list = [
 ## 1
-'The updated Roadmap is published on the forum at the below link.\
-\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195',
+"The updated Roadmap is published on the forum at the below link.\
+\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195",
 ## 2
-'Article, 10 May 2018, Overview of White Paper 2.0 Part 1\
+"Article, 10 May 2018, Overview of White Paper 2.0 Part 1\
 \nhttps://boscoin.io/article/overview-of-white-paper-2-part1\
 \nIf you have not read this, it is a good overview of the white\
- paper 2.0 in compliance review.',
+ paper 2.0 in compliance review.",
 ## 3
-'If you are not already subscribed to the Official BOScoin\
+"If you are not already subscribed to the Official BOScoin\
  Announcements Telegram now is a great time. Receive all the\
  important announcements in an uncluttered channel.\
-\nhttps://t.me/OfficialBOScoinAnnouncements',
+\nhttps://t.me/OfficialBOScoinAnnouncements",
 ## 4
-'We have published a one page brochure covering basic platform information.\
-\nhttps://boscoin.io/data/brochure.html',
+"We have published a one page brochure covering basic platform information.\
+\nhttps://boscoin.io/data/brochure.html",
 ## 5
-'You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
+"You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
  the community. The exact procedure, and more information will be released as we\
- get closer to MainNet Launch near the end of Q4, 2018.\
-\nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe',
+ get closer to MainNet Launch, which will be in Nov/Dec 2018.\
+\nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe",
 ## 6
-'Follow the development on github by “watching” the repository\
+"Follow the development on github by “watching” the repository\
 \nhttps://github.com/bosnet/sebak\
-\nselecting email, and you’ll receive all the development updates.',
+\nselecting email, and you’ll receive all the development updates.",
 ## 7
-'Article and Application, "Reverse ICO Partner Program"\
+"Article and Application, 'Reverse ICO Partner Program'\
 \nhttps://boscoin.io/pf/\
 \n\
 What is RIPP (Reverse ICO Partner Program)?\
@@ -40,54 +40,74 @@ What is RIPP (Reverse ICO Partner Program)?\
 \n- Our partners can conduct business quickly, without the need for extensive\
  technological investments into the blockchain. Furthermore, raising post-ICO\
  capital on BOSNet through Public Financing is drastically cheaper than using\
- the traditional financial system with its high fees.',
+ the traditional financial system with its high fees.",
 ## 8
-'We have published a one page brochure covering basic platform information.\
+"We have published a one page brochure covering basic platform information.\
 \nhttps://boscoin.io/data/brochure.html\
 \n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
 \n- BOScoin offers three alternatives - credit creation through participation,\
  democratic impact investments and communization of certain assets to form a new\
- "Project Financing (PF)" concept to power better capitalism, solving the high\
+ 'Project Financing (PF)'' concept to power better capitalism, solving the high\
  interest rate problem in conventional PF.\
-\n- BOScoin plans to release "BOSnet", a blockchain based mainnet optimized for\
- PF by Q4 of 2018.\
-\n- BOScoin is running a "Reverse ICO Partner Program" to discover and foster\
- companies in need of PF through reverse ICOs.',
+\n- BOScoin plans to release 'BOSnet', a blockchain based mainnet optimized for\
+ PF scheduled for Nov/Dec 2018.\
+\n- BOScoin is running a 'Reverse ICO Partner Program' to discover and foster\
+ companies in need of PF through reverse ICOs.",
 ## 9
-'Exchanges\
-\n- Hitbtc - Do not buy from this exchange, they are trading an "IOU" withdrawals\
+"Exchanges\
+\n- Hitbtc - Do not buy from this exchange, they are trading an 'IOU' withdrawals\
  and deposits are not allowed.\
 \n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.co.kr are the\
- only exchanges trading BOScoin at this time.',
+ only exchanges trading BOScoin at this time.",
 ## 10
-'Monthly Ask Me Anything with CEO and CTO\
+"Monthly Ask Me Anything with CEO and CTO\
 \nSave your questions and submit them when the question period opens on the 1st.\
 \n- Submit questions 1st -10th\
 \n- Team compiles answers 11th - 20th\
 \n- Publishes answers on boscoin.io 21st - 31st\
-\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform',
+\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform",
 ## 11
-'Article, 10 May 2018, "Overview of White Paper 2.0"\
-\nhttps://boscoin.io/article/overview-of-white-paper-2-part1/\
-\nIf you have not read this, it is a good overview of the white paper 2.0 in\
- compliance review.',
-## 12
-'The updated Roadmap is published on the forum at the below link.\
-\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195',
-## 13
-'We have several official channels for up to date information on our project. We\
+"We have several official channels for up to date information on our project. We\
  recommend you use these to stay informed regarding current events in the BOScoin\
  eco-system.\
 \n\
-\n- Twitter www.twitter.com/boscoinio\
+\n- Twitter - www.twitter.com/boscoinio\
 \n- Community Forum - https://forum.boscoin.io/\
 \n- Telegram announcements - https://t.me/OfficialBOScoinAnnouncements\
 \n- Medium Blog - https://medium.com/@boscoin\
 \n- Github repositories - https://github.com/bosnet\
-\n- Official Youtube channel - https://www.youtube.com/channel/UC8jcLGbwx_itHzgFPcTLiBg',
-## 14
-'We have released the TokenNet Android Mobile Wallet on the Google Play store.\
+\n- Official Youtube channel - https://www.youtube.com/channel/UC8jcLGbwx_itHzgFPcTLiBg",
+## 12
+"We have released the TokenNet Android Mobile Wallet on the Google Play store.\
 \nhttps://play.google.com/store/apps/details?id=io.boscoin.toknenet.wallet\
 \n\
-\nThe iOS version is scheduled for Q1, 2019.'
+\nThe iOS version is scheduled for Q1, 2019.",
+## 13
+"Mainnet (Sebak) is on schedule, and we will be releasing information on Open\
+ Membership, KYC, and Mainnet launch in the coming weeks. You can receive all\
+ the latest updates on our announcements channel.\
+ /nhttps://t.me/OfficialBOScoinAnnouncements",
+ ## 14
+ "Our CEO, Yezune Choi, was featured in a cover story 'BOScoin: Reaching the New\
+  Heights of a Cryptocurrency Framework'\
+ \nhttps://www.insightssuccess.com/boscoin-reaching-the-new-heights-of-a-cryptocurrency-framework/",
+ ## 15
+ "CTO, Minhyo Bae\
+\nMinhyo has been a software developer for many years. He started with BlockchainOS\
+ as a blockchain trust contract developer, but quickly became our lead developer\
+ implementing our CEO, Yezune Choi's vision for the BOScoin platform. Minhyo has\
+ extensive experience in building and managing large distributed networks in cloud\
+ systems like AWS, Azure and GCP.\
+\nhttps://boscoin.io/about/",
+## 16
+"Meet our CEO, Yezune Choi\
+\nYezune is a top expert in blockchain technology. He has over 20 years experience\
+ in software engineering, and has been devoted to the development of blockchain\
+ technology for the last five years.\
+\nhttps://boscoin.io/about/",
+## 17
+"BOScoin has proposed a new solution to solve the high financial commission\
+ problem of Project Financing through ‘Public Financing’ and implemented with\
+ a program called RIPP (Reverse ICO Partner Program).\
+ \nhttps://boscoin.io/pf/"
 ]
