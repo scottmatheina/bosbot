0854abce65cb070ce98b39c47f8f79e72400d82f
Scott Matheina <scott@matheina.com>
2018-10-28 09:12:43 -0500

    update welcome messages

--- msg_list.py
+++ msg_list.py
@@ -3,13 +3,13 @@
 
 message_list = [
 ## 1
-'The updated Roadmap is published on the forum at the below\
- link. https://forum.boscoin.io/t/boscoin-roadmap/2195',
+'The updated Roadmap is published on the forum at the below link.\
+\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195',
 ## 2
 'Article, 10 May 2018, Overview of White Paper 2.0 Part 1\
 \nhttps://boscoin.io/article/overview-of-white-paper-2-part1\
 \nIf you have not read this, it is a good overview of the white\
- paper 2.0 part 1 in compliance review.',
+ paper 2.0 in compliance review.',
 ## 3
 'If you are not already subscribed to the Official BOScoin\
  Announcements Telegram now is a great time. Receive all the\
@@ -32,30 +32,33 @@ message_list = [
 \nhttps://boscoin.io/pf/\
 \n\
 What is RIPP (Reverse ICO Partner Program)?\
-\n- RIPP is financed by BOSNet, the mainnet for public financing based on blockchain.\
-\n- RIPP is targeting reverse-ICO firms that are already operational with their own business\
- models, and looking for additional financing through blockchain based ICOs or Public Financing,\
- not the traditional financial system.\
-\n- Our partners can conduct business quickly, without the need for extensive technological\
- investments into the blockchain. Furthermore, raising post-ICO capital on BOSNet through\
- Public Financing is drastically cheaper than using the traditional financial system with\
- its high fees.',
+\n- RIPP is financed by BOSNet, the mainnet for public financing based on\
+ blockchain.\
+\n- RIPP is targeting reverse-ICO firms that are already operational with their\
+ own business models, and looking for additional financing through blockchain\
+ based ICOs or Public Financing, not the traditional financial system.\
+\n- Our partners can conduct business quickly, without the need for extensive\
+ technological investments into the blockchain. Furthermore, raising post-ICO\
+ capital on BOSNet through Public Financing is drastically cheaper than using\
+ the traditional financial system with its high fees.',
 ## 8
 'We have published a one page brochure covering basic platform information.\
 \nhttps://boscoin.io/data/brochure.html\
 \n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
-\n- BOScoin offers three alternatives - credit creation through participation, democratic impact\
- investments and communization of certain assets to form a new "Project Financing (PF)" concept to\
- power better capitalism, solving the high interest rate problem in conventional PF.\
-\n- BOScoin plans to release "BOSnet", a blockchain based mainnet optimized for PF by Q4 of 2018.\
-\n- BOScoin is running a "Reverse ICO Partner Program" to discover and foster companies in need of\
- PF through reverse ICOs.',
+\n- BOScoin offers three alternatives - credit creation through participation,\
+ democratic impact investments and communization of certain assets to form a new\
+ "Project Financing (PF)" concept to power better capitalism, solving the high\
+ interest rate problem in conventional PF.\
+\n- BOScoin plans to release "BOSnet", a blockchain based mainnet optimized for\
+ PF by Q4 of 2018.\
+\n- BOScoin is running a "Reverse ICO Partner Program" to discover and foster\
+ companies in need of PF through reverse ICOs.',
 ## 9
 'Exchanges\
-\n- Hitbtc - Do not buy from this exchange, they are trading an "IOU" withdrawals and deposits\
- are not allowed.\
-\n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.co.kr are the only exchanges\
- trading BOScoin at this time.',
+\n- Hitbtc - Do not buy from this exchange, they are trading an "IOU" withdrawals\
+ and deposits are not allowed.\
+\n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.co.kr are the\
+ only exchanges trading BOScoin at this time.',
 ## 10
 'Monthly Ask Me Anything with CEO and CTO\
 \nSave your questions and submit them when the question period opens on the 1st.\
@@ -66,13 +69,15 @@ What is RIPP (Reverse ICO Partner Program)?\
 ## 11
 'Article, 10 May 2018, "Overview of White Paper 2.0"\
 \nhttps://boscoin.io/article/overview-of-white-paper-2-part1/\
-\nIf you have not read this, it is a good overview of the white paper 2.0 in compliance review.',
+\nIf you have not read this, it is a good overview of the white paper 2.0 in\
+ compliance review.',
 ## 12
 'The updated Roadmap is published on the forum at the below link.\
-\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195'
+\nhttps://forum.boscoin.io/t/boscoin-roadmap/2195',
 ## 13
-'We have several official channels for up to date information on our project. We recommend you use\
- these to stay informed regarding current events in the BOScoin eco-system.\
+'We have several official channels for up to date information on our project. We\
+ recommend you use these to stay informed regarding current events in the BOScoin\
+ eco-system.\
 \n\
 \n- Twitter www.twitter.com/boscoinio\
 \n- Community Forum - https://forum.boscoin.io/\
