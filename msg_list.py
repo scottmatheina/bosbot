4a00e9fadd215498a906004a8ac9d7e1f3b9d6e9
Scott Matheina <scott@matheina.com>
2018-11-01 08:30:16 -0500

    update message list

--- msg_list.py
+++ msg_list.py
@@ -2,32 +2,32 @@
 # -*- coding: utf-8 -*-
 
 message_list = [
-## 1
+##
 "The updated Roadmap is published on the forum at the below link.\
 \nhttps://forum.boscoin.io/t/boscoin-roadmap/2195",
-## 2
+##
 "Article, 10 May 2018, Overview of White Paper 2.0 Part 1\
 \nhttps://boscoin.io/article/overview-of-white-paper-2-part1\
 \nIf you have not read this, it is a good overview of the white\
  paper 2.0 in compliance review.",
-## 3
+##
 "If you are not already subscribed to the Official BOScoin\
  Announcements Telegram now is a great time. Receive all the\
  important announcements in an uncluttered channel.\
 \nhttps://t.me/OfficialBOScoinAnnouncements",
-## 4
+##
 "We have published a one page brochure covering basic platform information.\
 \nhttps://boscoin.io/data/brochure.html",
-## 5
+##
 "You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
  the community. The exact procedure, and more information will be released as we\
  get closer to MainNet Launch, which will be in Nov/Dec 2018.\
 \nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe",
-## 6
+##
 "Follow the development on github by “watching” the repository\
 \nhttps://github.com/bosnet/sebak\
 \nselecting email, and you’ll receive all the development updates.",
-## 7
+##
 "Article and Application, 'Reverse ICO Partner Program'\
 \nhttps://boscoin.io/pf/\
 \n\
@@ -41,7 +41,7 @@ What is RIPP (Reverse ICO Partner Program)?\
  technological investments into the blockchain. Furthermore, raising post-ICO\
  capital on BOSNet through Public Financing is drastically cheaper than using\
  the traditional financial system with its high fees.",
-## 8
+##
 "We have published a one page brochure covering basic platform information.\
 \nhttps://boscoin.io/data/brochure.html\
 \n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
@@ -53,19 +53,25 @@ What is RIPP (Reverse ICO Partner Program)?\
  PF scheduled for Nov/Dec 2018.\
 \n- BOScoin is running a 'Reverse ICO Partner Program' to discover and foster\
  companies in need of PF through reverse ICOs.",
-## 9
+##
 "Exchanges\
 \n- Hitbtc - Do not buy from this exchange, they are trading an 'IOU' withdrawals\
  and deposits are not allowed.\
 \n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.co.kr are the\
  only exchanges trading BOScoin at this time.",
-## 10
+##
 "Monthly Ask Me Anything with CEO and CTO\
 \nQuestion period is open now, and closes on the 10th.\
 \n- Submit questions 1st -10th\
 \n- Team compiles answers 11th - 20th\
 \n- Publishes answers on boscoin.io 21st - 31st\
 \nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform",
+##
+"You can read our October AMA at the following links:\
+\nhttps://boscoin.io/ask-me-anything-development-october/\
+\nhttps://boscoin.io/ask-me-anything-other-october/\
+\nhttps://boscoin.io/ask-me-anything-partnerships-agreements-and-services-october/\
+\nhttps://boscoin.io/ask-me-anything-schedule-october/",
 ## 11
 "We have several official channels for up to date information on our project. We\
  recommend you use these to stay informed regarding current events in the BOScoin\
@@ -77,21 +83,21 @@ What is RIPP (Reverse ICO Partner Program)?\
 \n- Medium Blog - https://medium.com/@boscoin\
 \n- Github repositories - https://github.com/bosnet\
 \n- Official Youtube channel - https://www.youtube.com/channel/UC8jcLGbwx_itHzgFPcTLiBg",
-## 12
+##
 "We have released the TokenNet Android Mobile Wallet on the Google Play store.\
 \nhttps://play.google.com/store/apps/details?id=io.boscoin.toknenet.wallet\
 \n\
 \nThe iOS version is scheduled for Q1, 2019.",
-## 13
+##
 "Mainnet (Sebak) is on schedule, and we will be releasing information on Open\
  Membership, KYC, and Mainnet launch in the coming weeks. You can receive all\
  the latest updates on our announcements channel.\
  \nhttps://t.me/OfficialBOScoinAnnouncements",
- ## 14
+ ##
  "Our CEO, Yezune Choi, was featured in a cover story 'BOScoin: Reaching the New\
   Heights of a Cryptocurrency Framework'\
  \nhttps://www.insightssuccess.com/boscoin-reaching-the-new-heights-of-a-cryptocurrency-framework/",
- ## 15
+ ##
  "CTO, Minhyo Bae\
 \nMinhyo has been a software developer for many years. He started with BlockchainOS\
  as a blockchain trust contract developer, but quickly became our lead developer\
@@ -99,13 +105,25 @@ What is RIPP (Reverse ICO Partner Program)?\
  extensive experience in building and managing large distributed networks in cloud\
  systems like AWS, Azure and GCP.\
 \nhttps://boscoin.io/about/",
-## 16
+##
 "Meet our CEO, Yezune Choi\
 \nYezune is a top expert in blockchain technology. He has over 20 years experience\
  in software engineering, and has been devoted to the development of blockchain\
  technology for the last five years.\
 \nhttps://boscoin.io/about/",
-## 17
+##
+"Meet our CSO, Myungsan Jun\
+ Myungsan Jun graduated with a bachelor’s degree from the Division of Sociology,\
+ Seoul National University in Korea in 1995. He has worked in the IT industry for\
+ 20 years including Cyworld, the world’s first successful social network service (SNS).\
+ He published a book, in 2012, which analyzed the communication structure of twenty-first century.\
+ This book was selected as an “Excellent literary book” in 2012 by the Ministry of Culture\
+ and Tourism, Korea. He also writes IT columns for various journals in Korea. In 2017,\
+ he published his second book, in Korea in which he investigates the relationship between\
+ the government, blockchain and social trust.\
+\nhttps://boscoin.io/blockchain-government-free-download/\
+\nHe now is concentrating on BOScoin to redesign the future with blockchain technology.",
+##
 "BOScoin has proposed a new solution to solve the high financial commission\
  problem of Project Financing through ‘Public Financing’ and implemented with\
  a program called RIPP (Reverse ICO Partner Program).\
