fc23debb8a4e12df947bef7622e7e67c43109d51
Scott Matheina <scott@matheina.com>
2018-11-10 11:53:59 -0600

    update

--- msg_list.py
+++ msg_list.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 message_list = [
-##
+
+#################################################
+#						#
+# General Announcements - Updated Rarely 	#
+#						#
+#################################################
+
 "The updated Roadmap is published on the forum at the below link.\
 \nhttps://forum.boscoin.io/t/boscoin-roadmap/2195",
 ##
@@ -11,62 +17,10 @@ message_list = [
  important announcements in an uncluttered channel.\
 \nhttps://t.me/OfficialBOScoinAnnouncements",
 ##
-"You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
- the community. The exact procedure, and more information will be released as we\
- get closer to MainNet Launch, which will be in Nov 15, 2018.\
-\nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe",
-##
 "Follow the development on github by “watching” the repository\
 \nhttps://github.com/bosnet/sebak\
 \nselecting email, and you’ll receive all the development updates.",
 ##
-"Article and Application, 'Reverse ICO Partner Program'\
-\nhttps://boscoin.io/pf/\
-\n\
-What is RIPP (Reverse ICO Partner Program)?\
-\n- RIPP is financed by BOSNet, the mainnet for public financing based on\
- blockchain.\
-\n- RIPP is targeting reverse-ICO firms that are already operational with their\
- own business models, and looking for additional financing through blockchain\
- based ICOs or Public Financing, not the traditional financial system.\
-\n- Our partners can conduct business quickly, without the need for extensive\
- technological investments into the blockchain. Furthermore, raising post-ICO\
- capital on BOSNet through Public Financing is drastically cheaper than using\
- the traditional financial system with its high fees.",
-##
-"We have published a one page brochure covering basic platform information.\
-\nhttps://boscoin.io/data/brochure.html\
-\n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
-\n- BOScoin offers three alternatives - credit creation through participation,\
- democratic impact investments and communization of certain assets to form a new\
- 'Project Financing (PF)' concept to power better capitalism, solving the high\
- interest rate problem in conventional PF.\
-\n- BOScoin plans to release 'BOSnet', a blockchain based mainnet optimized for\
- PF scheduled for Nov 15, 2018 launch.\
-\n- BOScoin is running a 'Reverse ICO Partner Program' to discover and foster\
- companies in need of PF through reverse ICOs.",
-##
-"Exchanges\
-\n- Hitbtc - Do not buy from this exchange, they are trading an 'IOU' withdrawals\
- and deposits are not allowed.\
-\n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.com are the\
- only exchanges trading BOScoin at this time.",
-##
-"Monthly Ask Me Anything with CEO and CTO\
-\nQuestion period is open now, and closes on the 10th.\
-\n- Submit questions 1st -10th\
-\n- Team compiles answers 11th - 20th\
-\n- Publishes answers on boscoin.io 21st - 31st\
-\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform",
-##
-"You can read our October AMA at the following links:\
-\nhttps://boscoin.io/ask-me-anything-development-october/\
-\nhttps://boscoin.io/ask-me-anything-other-october/\
-\nhttps://boscoin.io/ask-me-anything-partnerships-agreements-and-services-october/\
-\nhttps://boscoin.io/ask-me-anything-schedule-october/\
-\nSubmit your own questions today:\
-\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform",
-## 11
 "We have several official channels for up to date information on our project. We\
  recommend you use these to stay informed regarding current events in the BOScoin\
  eco-system.\
@@ -77,16 +31,13 @@ What is RIPP (Reverse ICO Partner Program)?\
 \n- Medium Blog - https://medium.com/@boscoin\
 \n- Github repositories - https://github.com/bosnet\
 \n- Official Youtube channel - https://www.youtube.com/channel/UC8jcLGbwx_itHzgFPcTLiBg",
-##
-"We have released the TokenNet Android Mobile Wallet on the Google Play store.\
-\nhttps://play.google.com/store/apps/details?id=io.boscoin.toknenet.wallet\
-\nThere will be a Mainnet Android Mobile Wallet released\
-\nThe iOS version is scheduled for Q1, 2019.",
-##
-"Mainnet (Sebak) is on schedule for Nov 15, 2018 launch. You can receive all\
- the latest updates on our announcements channel.\
-\nhttps://t.me/OfficialBOScoinAnnouncements",
- ##
+
+#################################################
+#						#
+# Team Introductions 				#
+#						#
+#################################################
+
 "Our CEO, Yezune Choi, was featured in a cover story 'BOScoin: Reaching the New\
  Heights of a Cryptocurrency Framework'\
 \nhttps://www.insightssuccess.com/boscoin-reaching-the-new-heights-of-a-cryptocurrency-framework/",
@@ -122,28 +73,26 @@ What is RIPP (Reverse ICO Partner Program)?\
  a program called RIPP (Reverse ICO Partner Program).\
 \nhttps://boscoin.io/pf/",
 ##
-"[Real-Time Translation: English] 'Play' Instead of Work For Blockchain by Yezune\
- Choi, BOScoin CEO\
-\nhttps://youtu.be/UBtoOVus2A0",
-##
-"[Real-Time Translation: English] ABF Panel Discussion of Yezune Choi, BOScoin CEO\
- / Paul Kim\
-\nhttps://youtu.be/ZZ0EoeKM-h8",
-##
-"To celebrate Bitcoin’s 10th birthday, BOScoin decided to offer a free pdf download\
- of “Blockchain Government” written by Myungsan Jun from BOScoin.\
-\nhttps://boscoin.io/blockchain-government-free-download/",
-##
-"White Paper 2.0 is released\
-\nhttps://boscoin.io/about/#papers",
-##
-"Mainnet launch is November 15, 2018",
-##
-"We are coordinating with exchanges during our migration to Mainnet (Sebak),\
- it is recommended you transfer your BOScoin to a wallet - you hold the secret\
- seed. You will be able to access your BOScoins on our wallet.\
-\nhttps://wallet.boscoin.io",
-## Quotes from White Paper 2.0
+"Article and Application, 'Reverse ICO Partner Program'\
+\nhttps://boscoin.io/pf/\
+\n\
+What is RIPP (Reverse ICO Partner Program)?\
+\n- RIPP is financed by BOSNet, the mainnet for public financing based on\
+ blockchain.\
+\n- RIPP is targeting reverse-ICO firms that are already operational with their\
+ own business models, and looking for additional financing through blockchain\
+ based ICOs or Public Financing, not the traditional financial system.\
+\n- Our partners can conduct business quickly, without the need for extensive\
+ technological investments into the blockchain. Furthermore, raising post-ICO\
+ capital on BOSNet through Public Financing is drastically cheaper than using\
+ the traditional financial system with its high fees.",
+
+#################################################
+#						#
+# Quotes from White Paper 2.0 			#
+#						#
+#################################################
+
 "Public Financing creates credit, not through decisions made by a central bank or government,\
  but through the collective consensus of individuals who use and trade real credit.\
  BOScoin has already introduced a governance system called the Congress Network to\
@@ -221,9 +170,15 @@ What is RIPP (Reverse ICO Partner Program)?\
  a governance system called the Congress Network.\
 \nhttps://boscoin.io/wp-content/themes/boscoin/src/pdf/BOScoinWhitePaper_2.0_EN.pdf\
  (pg. 15)",
- ## Press Releases from PRnewswire.com
- "Press Release: Nov 2, 2018: Ahead of MainNet Launch on Nov. 15, BOScoin Releases Updated White\
-  Paper 2.0: Focused on Mainnet Operating Principles\
+
+#################################################
+#						#
+# Press Releases from PRnewswire 		#
+#						#
+#################################################
+
+"Press Release: Nov 2, 2018: Ahead of MainNet Launch on Nov. 15, BOScoin Releases Updated White\
+ Paper 2.0: Focused on Mainnet Operating Principles\
 \nhttps://www.prnewswire.com/news-releases/ahead-of-mainnet-launch-on-nov-15--boscoin-releases-updated-white-paper-2-0-focused-on-mainnet-operating-principles-300743048.html",
 ##
 "Press Release: Oct 22, 2018: BOScoin's Partner 'Nature Mobility' launches Zzimcar Direct for small\
@@ -246,27 +201,66 @@ What is RIPP (Reverse ICO Partner Program)?\
 ##
 "Press Release: Nov 9, 2018: BOScoin Announces First Voting Results at BOSCON 2018\
 \nhttps://www.prnewswire.com/news-releases/boscoin-announces-first-voting-results-at-boscon-2018-300747429.html",
-##
+
+#################################################
+#						#
+# Articles published on BOScoin.io 		#
+#						#
+#################################################
+
 "Article: Nov 5, 2018: Introduction of ISAAC consensus protocol betterments\
 \nhttps://bosnet.github.io/introduction-ISAAC-consensus-Betterment.html",
-##
-"Announcement: Participation Appreciation for BOScoin’s First Voting\
-\nBOScoin’s very first voting event for BOScoin members.\
- \nThe purpose of this voting is to decide whether the Membership Reward proposed by\
- the BOS team is favourable for its members. As a BOScoin member, you get to vote\
- 'Agree', 'Disagree', or 'Abstain' to the Membership Reward Proposal. The result\
- will be announced right after the voting ends on December 6th, 2018. BOScoin would\
- like to thank you for participating in this voting event; therefore, we prepared\
- some appreciation for you.\
-\nhttps://boscoin.io/appreciation/",
-##
+
+#################################################
+#						#
+# Videos published on the official Youtube 	#
+#						#
+#################################################
+
 "Video: [English Subtitle] BOScoin “Membership Reward PF (Public Financing) by Yezune\
  Choi\
 \nhttps://youtu.be/VGPt9yJXsfI",
 ##
-"Join, Vote, and Receive up to 1,000 #BOScoin! Voting starts Nov 30\
-\nhttps://boscoin.io/appreciation/\
-\nhttps://youtu.be/amQyXwjvC0o",
+"[Real-Time Translation: English] ABF Panel Discussion of Yezune Choi, BOScoin CEO\
+ / Paul Kim\
+\nhttps://youtu.be/ZZ0EoeKM-h8",
+##
+"[Real-Time Translation: English] 'Play' Instead of Work For Blockchain by Yezune\
+ Choi, BOScoin CEO\
+\nhttps://youtu.be/UBtoOVus2A0",
+
+#################################################
+#						#
+# General Announcements - Updated Often 	#
+# 						#
+#################################################
+
+##
+"To celebrate Bitcoin’s 10th birthday, BOScoin decided to offer a free pdf download\
+ of “Blockchain Government” written by Myungsan Jun from BOScoin.\
+\nhttps://boscoin.io/blockchain-government-free-download/",
+##
+"Exchanges\
+\n- Hitbtc - Do not buy from this exchange, they are trading an 'IOU' withdrawals\
+ and deposits are not allowed.\
+\n- Coinbene.com, Kucoin.com, 2xchange.co.kr, LATOKEN.com and gdac.com are the\
+ only exchanges trading BOScoin at this time.",
+##
+"We have released the TokenNet Android Mobile Wallet on the Google Play store.\
+\nhttps://play.google.com/store/apps/details?id=io.boscoin.toknenet.wallet\
+\nThere will be a Mainnet Android Mobile Wallet released\
+\nThe iOS version is scheduled for Q1, 2019.",
+##
+"Mainnet (Sebak) is on schedule for Nov 15, 2018 launch. You can receive all\
+ the latest updates on our announcements channel.\
+\nhttps://t.me/OfficialBOScoinAnnouncements",
+##
+"Mainnet launch is November 15, 2018",
+##
+"We are coordinating with exchanges during our migration to Mainnet (Sebak),\
+ it is recommended you transfer your BOScoin to a wallet - you hold the secret\
+ seed. You will be able to access your BOScoins on our wallet.\
+\nhttps://wallet.boscoin.io",
 ##
 "On Mainnet, Launching Nov 15th, you will be able to freeze BOScoin with the\
  BOScoin Mobile Wallet for Android or the Desktop Wallet, but to vote on proposals,\
@@ -274,12 +268,60 @@ What is RIPP (Reverse ICO Partner Program)?\
 \nhttps://github.com/bosnet/sebak/wiki/SEBAK-Installation-Guide\
 \nhttps://github.com/bosnet/sebak/wiki/SEBAK-Commands#sebak-wallet",
 ##
-"Have you heard of Participation appreciation?\
-\nVote and you could win up to 1000 BOS?\
+"We have published a one page brochure covering basic platform information.\
+\nhttps://boscoin.io/data/brochure.html\
+\n- BOScoin is a finance blockchain project to hack capitalism in the most capitalist way.\
+\n- BOScoin offers three alternatives - credit creation through participation,\
+ democratic impact investments and communization of certain assets to form a new\
+ 'Project Financing (PF)' concept to power better capitalism, solving the high\
+ interest rate problem in conventional PF.\
+\n- BOScoin plans to release 'BOSnet', a blockchain based mainnet optimized for\
+ PF scheduled for Nov 15, 2018 launch.\
+\n- BOScoin is running a 'Reverse ICO Partner Program' to discover and foster\
+ companies in need of PF through reverse ICOs.",
+ ##
+"White Paper 2.0 is released\
+\nhttps://boscoin.io/about/#papers",
+"You can review the Proposed MainNet Rewards Schedule, this will be voted on by\
+ the community. The exact procedure, and more information will be released as we\
+ get closer to MainNet Launch, which will be in Nov 15, 2018.\
+\nVoting starts on Nov 30 - Dec 6\
+\nhttps://medium.com/boscoin/bos-newsletter-august-28th-2018-f3ba33102efe",
+##
+"Monthly Ask Me Anything with CEO and CTO\
+\nQuestion period is open now, and closes on the 10th.\
+\n- Submit questions 1st -10th\
+\n- Team compiles answers 11th - 20th\
+\n- Publishes answers on boscoin.io 21st - 31st\
+\nhttps://docs.google.com/forms/d/e/1FAIpQLSdjFPWZPAEPKeuGP7hl7Wp02QgCpZow0bA5b2UZVBlY7-TQKA/viewform",
+
+#################################################
+#						#
+# Promotion Announcements 			#
+#						#
+#################################################
+
+"Join, Vote, and Receive up to 1,000 #BOScoin! Voting starts Nov 30\
+\nhttps://boscoin.io/appreciation/\
+\nhttps://youtu.be/amQyXwjvC0o",
+##
+"Have you heard of BOScoin Voting participation appreciation?\
+\nVote and you could earn up to 1000 BOS?\
 \n\
 \n1000 BOS (between Nov 30th & Dec 2nd)\
 \n500 BOS (between Dec 2nd & Dec 4th)\
 \n250 BOS (between Dec 4th & Dec 6th)\
 \n\
-https://boscoin.io/appreciation/"
+\nhttps://boscoin.io/appreciation/",
+ ##
+"Participation Appreciation for BOScoin’s First Voting\
+\nBOScoin’s very first voting event for BOScoin members.\
+ \nThe purpose of this voting is to decide whether the Membership Reward proposed by\
+ the BOS team is favourable for its members. As a BOScoin member, you get to vote\
+ 'Agree', 'Disagree', or 'Abstain' to the Membership Reward Proposal. The result\
+ will be announced right after the voting ends on December 6th, 2018. BOScoin would\
+ like to thank you for participating in this voting event; therefore, we prepared\
+ some appreciation for you.\
+\nhttps://boscoin.io/appreciation/"
+
 ]
